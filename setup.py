0a0234661dcd9096bbba351b3fce2fc6d0d319d9
Randy Heydon <randy.heydon@clockworklab.net>
2011-02-25 17:48:51 -0500

    Added license and other metadata so I can upload.
    Also noted licenses of testfiles in README.txt, and took the opportunity to
    retab setup.py.

--- setup.py
+++ setup.py
@@ -2,13 +2,15 @@
 from distutils.core import setup
 
 setup(
-	name = 'PNDstore',
-	version = '0',
-	description = 'thing', #TODO: a real description
-	author = 'Randy Heydon',
-	author_email = 'randy.heydon@clockworklab.net',
-	url = 'http://randy.heydon.selfip.net/Programs/PNDstore/', #TODO: check url
-	packages = ['pndstore', 'pndstore_gui'],
-    package_data = {'pndstore': ['cfg/*']}
-	scripts = ['PNDstore', 'pndst'],
+    name = 'PNDstore',
+    version = '0',
+    description = 'thing', #TODO: a real description
+    long_description = open('README.txt').read(),
+    author = 'Randy Heydon',
+    author_email = 'randy.heydon@clockworklab.net',
+    url = 'http://randy.heydon.selfip.net/Programs/PNDstore/', #TODO: check url
+    packages = ['pndstore', 'pndstore_gui'],
+    package_data = {'pndstore': ['cfg/*']},
+    scripts = ['PNDstore', 'pndst'],
+    license = 'LGPL',
 )
