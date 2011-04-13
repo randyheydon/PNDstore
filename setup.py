b83d31fb68b3a3590482ae73ef9de1574d76ddf4
Randy Heydon <randy.heydon@clockworklab.net>
2011-04-13 17:29:15 -0400

    Added previewpic, prepared for bugfix release.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name = 'PNDstore',
-    version = '0.2.1',
+    version = '0.2.2',
     description = 'Install and update PNDs',
     long_description = open('README.txt').read(),
     author = 'Randy Heydon',
