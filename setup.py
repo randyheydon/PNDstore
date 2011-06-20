86503ff0c958d15d09d6ffd904cac8729c750aad
Randy Heydon <randy.heydon@clockworklab.net>
2011-06-20 17:37:07 -0400

    Moved error printing in GUI to a subfunction.
    There were four places that would show errors on the statusbar, mostly with the
    same logic.  This was moved to a method of the PNDstore class.  This method was
    also changed from the original code in that it also prints the errors to
    stdout, thereby ensuring that they'll end up in the .out file when run as PND.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name = 'PNDstore',
-    version = '1.0.1',
+    version = '1.0.2',
     description = 'Install and update PNDs',
     long_description = open('README.rst').read(),
     author = 'Randy Heydon',
