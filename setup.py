11c42f843a3963f650ddd32cd97e4af05e39bab7
Randy Heydon <randy.heydon@clockworklab.net>
2013-03-20 06:19:08 +0000

    Version number bump for bugfix release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name = 'PNDstore',
-    version = '1.0.3',
+    version = '1.0.4',
     description = 'Install and update PNDs',
     long_description = open('README.rst').read(),
     author = 'Randy Heydon',
